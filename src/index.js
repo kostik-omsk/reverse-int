bd8ece462958f9eeb3c01f6470cbf32e2987cc58
sidorenko konstantin <kosiak5587@gmail.com>
2023-01-03 20:54:15 +0600

    feat:  function ready

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let num = String(n > 0 ? n : Math.abs(n));
+    return num.split("").reverse().join("");
+};
+// reverse(-261);
